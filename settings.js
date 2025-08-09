d10d5b19ed2cfa46752b24c92e01272eb17a3368
Jo-byte-hub <okoyejoshua91@gmail.com>
2025-08-09 21:38:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: '',
-  author: '‎',
-  botName: ".",
-  botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  packname: '2349115983460',
+  author: 'Josh‎',
+  botName: "Joshua.",
+  botOwner: 'JOSH TECH', // Your name
+  ownerNumber: '2349115983460', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.0",
 };
